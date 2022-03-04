97cdebed8b5ba41675a84dab0fc24c46f23661a1
Mugisha-isaac <mugishaisaac2001@gmail>
2022-03-04 23:38:20 +0200

    working on signup page

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      builder: () => MaterialApp(
+        title: "Flutter Demo",
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          textTheme: GoogleFonts.nunitoTextTheme(
+            Theme.of(context).textTheme,
+          ),
+        ),
+        home: HomePage(),
+        debugShowCheckedModeBanner: false,
+      ),
+      designSize: const Size(360, 640),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final purpleColor = Color(0xff6688FF);
+  final darkTextColor = Color(0Xff1F1A3D);
+  final lightTextColor = Color(0Xff999999);
+  final textFieldColor = Color(0XffF5F6FA);
+  final borderColor = Color(0XffD9D9D9);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 52.h),
+              Text(
+                "Sign Up to Masterminds",
+                style: TextStyle(
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.w700,
+                    color: darkTextColor),
+              ),
+              SizedBox(height: 4.h),
+              Wrap(
+                children: [
+                  Text(
+                    "Already have an account?",
+                    style: TextStyle(
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w600,
+                        color: lightTextColor),
+                  ),
+                  Text(
+                    "Login",
+                    style: TextStyle(
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w700,
+                        color: purpleColor),
+                  ),
+                ],
+              ),
+              SizedBox(height: 24.h),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.r),
+                    borderSide:
+                        const BorderSide(color: Colors.transparent, width: 0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.r),
+                    borderSide:
+                        const BorderSide(color: Colors.transparent, width: 0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
